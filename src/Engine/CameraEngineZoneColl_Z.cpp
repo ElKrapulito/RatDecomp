1cc600fbb3a484e5e8b90eda40154f11f84c03e2
Mhanna112-code <Marc.Hanna-1@ou.edu>
2026-04-13 19:25:21 -0500

    matched CameraEngineZoneColl

---
+++ src/Engine/CameraEngineZoneColl_Z.cpp
@@ -0,0 +1,4 @@
+#include "CameraEngineZoneColl_Z.h"
+void CameraEngineZone_Z::CameraGameColl(const World_Z*, Vec3f, Vec3f pos, float) {
+    this->position = pos;
+}
--- src/Engine/includes/CameraEngineZoneColl_Z.h
+++ src/Engine/includes/CameraEngineZoneColl_Z.h
@@ -2,3 +2,18 @@
 #define _CAMERAENGINEZONECOLL_Z_H_
 #include "Types_Z.h"
 #endif // _CAMERAENGINEZONECOLL_Z_H_
+#include "World_ZHdl.h"
+#include "AnimationNode_Z.h"
+#include "AnimationMesh_Z.h"
+#include "AnimationMaterial_Z.h"
+#include "AnimationMorph_Z.h"
+#include "ResourceObject_Z.h"
+#include "BoneNode_Z.h"
+
+
+class CameraEngineZone_Z {
+public:
+    Vec3f position;  // must be at offset 0
+
+    void CameraGameColl(const World_Z*, Vec3f, Vec3f pos, float);
+};
