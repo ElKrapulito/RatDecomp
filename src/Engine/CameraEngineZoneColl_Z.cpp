d717d60849617efdc69b6a3bf9dda4a31bb2972b
Mhanna112-code <Marc.Hanna-1@ou.edu>
2026-04-14 14:32:53 -0500

    modified function to have correct Vec3f return type

--- src/Engine/CameraEngineZoneColl_Z.cpp
+++ src/Engine/CameraEngineZoneColl_Z.cpp
@@ -1,4 +1,9 @@
 #include "CameraEngineZoneColl_Z.h"
-void CameraEngineZone_Z::CameraGameColl(const World_Z*, Vec3f, Vec3f pos, float) {
-    this->position = pos;
+Vec3f CameraEngineZone_Z::CameraGameColl(
+    World_Z *i_World,
+    Vec3f i_CamPos,
+    Vec3f i_CamTarget,
+    float i_DeltaTime)
+{
+    return i_CamPos;
 }
--- src/Engine/includes/CameraEngineZoneColl_Z.h
+++ src/Engine/includes/CameraEngineZoneColl_Z.h
@@ -1,7 +1,6 @@
 #ifndef _CAMERAENGINEZONECOLL_Z_H_
 #define _CAMERAENGINEZONECOLL_Z_H_
 #include "Types_Z.h"
-#endif // _CAMERAENGINEZONECOLL_Z_H_
 #include "World_ZHdl.h"
 #include "AnimationNode_Z.h"
 #include "AnimationMesh_Z.h"
@@ -13,7 +12,11 @@
 
 class CameraEngineZone_Z {
 public:
-    Vec3f position;  // must be at offset 0
-
-    void CameraGameColl(const World_Z*, Vec3f, Vec3f pos, float);
+    Vec3f CameraGameColl(
+        World_Z *i_World,
+        Vec3f i_CamPos,
+        Vec3f i_CamTarget,
+        float i_DeltaTime
+    );
 };
+#endif // _CAMERAENGINEZONECOLL_Z_H_
